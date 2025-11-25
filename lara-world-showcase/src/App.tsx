8e97954811c32b6efa6fea74c0139436e0dc13f3
Jaydeep Sureliya <jaydeepsureliya.jd@gmail.com>
2025-11-25 18:47:18 +0530

    correction

--- lara-world-showcase/src/App.tsx
+++ lara-world-showcase/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/portal/login" element={<PortalLogin />} />
